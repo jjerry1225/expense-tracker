342e76670ea306f61a1edfe5524a703c998f951f
jjerry1225 <i183801225@gmail.com>
2023-02-16 10:50:24 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 引用連線mongoose的檔案，對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
+require("./config/mongoose")
+
 app.listen(PORT, () => {
   console.log(`Express is listening on http://localhost:${PORT}`)
 })
