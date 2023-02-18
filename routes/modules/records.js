14647a8d8f0e618e06a340945b2cb71964e03362
jjerry1225 <i183801225@gmail.com>
2023-02-18 15:38:03 +0800

    feat: add delete feature.

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -55,4 +55,16 @@ router.put("/:id", (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 刪除資料
+router.delete("/:id", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  Record.findOne({ _id, userId })
+    .then((record) => {
+      return record.remove()
+    })
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error))
+})
+
 module.exports = router;
