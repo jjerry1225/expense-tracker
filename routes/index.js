4aeccaac156be7506ba83337367bcb3beb27800d
jjerry1225 <i183801225@gmail.com>
2023-02-18 11:10:30 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // 引入模組程式碼
 const home = require("./modules/home")
 const records = require("./modules/records")
+const users = require("./modules/users")
 
 // 將網址結構符合要求字串的 request 導向對應模組
-router.use("/", home)
 router.use("/records", records)
+router.use("/users", users)
+router.use("/", home)
 
 module.exports = router
